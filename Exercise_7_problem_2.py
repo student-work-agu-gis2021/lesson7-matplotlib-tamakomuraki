559264aecfe99affb47524c607eb8fe6a4c361ed
tamakomuraki <>
2021-05-30 23:05:10 +0000

    I read the data into data and parse dates.

--- Exercise_7_problem_2.py
+++ Exercise_7_problem_2.py
@@ -17,6 +17,8 @@
 # - Parse dates from the column `'DATE'` and set the dates as index in the dataframe 
 
 # YOUR CODE HERE 1 to read the data into data and parse dates
+import pandas as pandasfp = "data/helsinki-vantaa.csv"
+data = pd.read_csv(fp,parse_dates['DATE'],index_col = 'DATE')
 
 # This test print should print first five rows
 print(data.head())
